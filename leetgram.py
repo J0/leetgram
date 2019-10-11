99cd9608f6ffedfae1e259a87e97a6d2a906c8ef
root <lee.yi.jie.joel@gmail.com>
2019-10-11 22:24:08 +0000

    Add caching logic

--- leetgram.py
+++ leetgram.py
@@ -1,8 +1,10 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext.dispatcher import run_async
 import requests
 import random
 import os
+import json
 
 updater = Updater(token=os.environ['TELEGRAM_KEY'], use_context=True)
 
@@ -12,15 +14,24 @@ def start(update, context):
 def help(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Commands available:\n/random - produces a random question\n\n To build:\n /interview - Display upcoming interviews\n /systems: Random System Design question. \n /calc - do math\n /happy - tell me a joke ")
 
+@run_async
 def happy(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Q: Why do the French like to eat snails?\nA: Because they hate fast food")
 
+@run_async
 def random_question(update, context):
     LEETCODE_LINK = "https://leetcode.com/problems/"
+    QUESTIONS_FILE = "questions.txt"
     NUM_QUESTIONS = 957
     r = requests.get('https://leetcode.com/api/problems/algorithms/')
+    if not os.path.exists(QUESTIONS_FILE):
+        r = requests.get('https://leetcode.com/api/problems/algorithms/')
+        with open(QUESTIONS_FILE, 'w+') as f:
+            f.write(json.dumps(r.json()))
     question_id = random.randint(0, NUM_QUESTIONS - 1)
-    question_title = r.json()["stat_status_pairs"][question_id]["stat"]["question__article__slug"]
+    with open(QUESTIONS_FILE, 'r+') as q:
+        question_bank_json = json.loads(q.read())
+    question_title = question_bank_json["stat_status_pairs"][question_id]["stat"]["question__article__slug"]
     context.bot.send_message(chat_id=update.message.chat_id, text="Here is your question of the day: {}".format(LEETCODE_LINK + question_title))
    
 dispatcher = updater.dispatcher
